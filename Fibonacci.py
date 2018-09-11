ded55796cfb6201ff6207fe7bc24c18082f958b0
hyesung <4538asd@naver.com>
2018-09-11 19:44:32 +0900

    .

---
+++ Fibonacci.py
@@ -0,0 +1,48 @@
+import time
+import random
+
+#반복적 피보나치 수
+
+def iterfibo(n):
+    if n <= 1:
+        return n
+    fiboF = 0
+    fiboL = 1
+    fibo = 0
+    for i in range(n - 1):
+        fibo = fiboF + fiboL
+        fiboF = fiboL
+        fiboL = fibo
+    return fibo
+
+
+fibo = 1
+fiboF = 1
+fiboL = 1
+
+fibo = 3
+
+
+def fibo(n):
+    if n <= 1:
+        return n
+    return fibo(n - 1) + fibo(n - 2)
+
+
+while True:
+    nbr = int(input('Enter a number:'))
+    if nbr == -1:
+        break
+
+    # 반복 피보나치
+    ts = time.time()
+    fibonumber = iterfibo(nbr)
+    ts = time.time() - ts
+    print('iterFibo(%d) = %d, time %.6f' % (nbr, fibonumber, ts))
+
+    # 재귀 피보나치
+
+    ts = time.time()
+    fibonumber = fibo(nbr)
+    ts = time.time() - ts
+    print('Fibo(%d) = %d, time %.6f' % (nbr, fibonumber, ts))
